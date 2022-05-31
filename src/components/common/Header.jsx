cf28f4a214a74c144aa63600094a4485535af20a
merge-simpson <letsdev@kakao.com>
2022-05-31 17:09:42 +0900

    Feat: Create header

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <h1>컴포넌트 연습</h1>
+      <nav>
+        <ul>
+          <li>로그인</li>
+          <li>투두리스트</li>
+          <li>ui-preview</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
