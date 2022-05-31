3dff947bd1b9e062ef556d0f39df29c3020ed68b
merge-simpson <letsdev@kakao.com>
2022-05-31 17:36:46 +0900

    Feat(Route): Add urls.
    - /
    - /login
    - /signup
    - /404
    - /* -> /404

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
@@ -8,7 +8,18 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes> {/* Switch~case 같은 역할 */}
+				{/* HOME */}
         <Route path="/" element={<App />} />
+
+				{/* USERS */}
+        <Route path="/login" element={<div>로그인 컴포넌트</div>} />
+        <Route path="/signup" element={<div>로그인 컴포넌트</div>} />
+				
+				{/* HTTP ERROR PAGE */}
+        <Route path="/404" element={<div>404</div>} />
+				
+        {/* Asterisk는 보통 Wildcard 역할 */}
+        <Route path="/*" element={<Navigate replace to="404" />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
